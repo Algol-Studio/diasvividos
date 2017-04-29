e3704fa0d5e6acdfed4ab987422afba42a332a56
Hsolteiroquertc <fabriciocf92@msn.com>
2017-04-29 16:59:14 -0300

    Começou

---
+++ app/src/main/java/algol/diasvividos/Nascimento_input.java
@@ -0,0 +1,13 @@
+package algol.diasvividos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Nascimento_input extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nascimento_input);
+    }
+}
