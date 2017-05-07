65c2693691b20e742a6322128a2e0abc8abe329a
Hsolteiroquertc <fabriciocf92@msn.com>
2017-05-06 22:09:22 -0300

    Fiquei um seculo perdendo tempo, mas sepa eu entendi +- o que não tava indo. Na real ainda acho que ta zuado, to comitando soh pra testar mas provavelmente eu ainda vou mudar tudo

--- app/src/main/java/algol/diasvividos/Nascimento_input.java
+++ app/src/main/java/algol/diasvividos/Nascimento_input.java
@@ -1,13 +1,142 @@
 package algol.diasvividos;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
 
-public class Nascimento_input extends AppCompatActivity {
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+public class Nascimento_input extends AppCompatActivity{
+
+    EditText Edit_Time = (EditText) findViewById(R.id.hora);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nascimento_input);
+
+    Edit_Time.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            // TODO Auto-generated method stub
+            Calendar mcurrentTime = Calendar.getInstance();
+            int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
+            int minute = mcurrentTime.get(Calendar.MINUTE);
+
+            TimePickerDialog mTimePicker;
+            mTimePicker = new TimePickerDialog(Nascimento_input.this, new TimePickerDialog.OnTimeSetListener() {
+                @Override
+                public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                    Edit_Time.setText( selectedHour + ":" + selectedMinute);
+                }
+            }, hour, minute, true);
+            mTimePicker.setTitle("Select Time");
+            mTimePicker.show();
+        }
+    }
+    );}
+
+/*    Calendar myCalendar = Calendar.getInstance();
+   // DatePicker DatePickerDialog = (DatePicker) findViewById(R.id.datePicker);
+    EditText edittext = (EditText) findViewById(R.id.data);
+
+    DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+            myCalendar.set(Calendar.YEAR, year);
+            myCalendar.set(Calendar.MONTH, monthOfYear);
+            myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            updateLabel();
+        }
+
+    };
+
+   edittext.setOnClickListener(new OnClickListener() {
+        //@Override
+        public void onClick(View view) {
+            new DatePickerDialog(classname.this, date, myCalendar
+                    .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                    myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+        }
+    });
+
+    private void updateLabel() {
+
+        String myFormat = "MM/dd/yy"; //In which you need put here
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        edittext.setText(sdf.format(myCalendar.getTime()));
+    }
+
+    EditText data,hora;
+    int year,month,day;
+
+    public void showDate(){
+        data = (EditText)findViewById(R.id.data);
+        data.setOnClickListener(
+                new View.OnClickListener(){
+                    @Override
+                    public void onClick(View V){
+                        aparecedia(findViewById(datePicker))
+                    }
+                }
+        );
     }
 }
+*/
+
+
+    public static double GetJulianDate(Calendar calendarDate){
+
+        int year = calendarDate.get(Calendar.YEAR);
+        int month = calendarDate.get(Calendar.MONTH) + 1;
+        int day = calendarDate.get(Calendar.DAY_OF_MONTH);
+        double hour = calendarDate.get(Calendar.HOUR_OF_DAY);
+        double minute = calendarDate.get(Calendar.MINUTE);
+        double second = calendarDate.get(Calendar.SECOND);
+        int isGregorianCal = 1;
+        int A;
+        int B;
+        int C;
+        int D;
+        double fraction = day + ((hour + (minute / 60) + (second / 60 / 60)) / 24);
+
+        if (year < 1582)
+        {
+            isGregorianCal = 0;
+        }
+
+        if (month < 3)
+        {
+            year = year - 1;
+            month = month + 12;
+        }
+
+        A = year / 100;
+        B = (2 - A + (A / 4)) * isGregorianCal;
+
+        if (year < 0)
+        {
+            C = (int)((365.25 * year) - 0.75);
+        }
+        else
+        {
+            C = (int)(365.25 * year);
+        }
+
+        D = (int)(30.6001 * (month + 1));
+        double JD = B + C + D + 1720994.5 + fraction;
+
+        return JD;
+}}
